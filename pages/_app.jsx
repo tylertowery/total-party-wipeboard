10e9710daea538559bed3e58597c9e6573230c76
Tyler Towery <tylertowery21@gmail.com>
2023-12-18 17:11:22 -0800

    adding favicon in right spot

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,5 @@
 import '../styles/global.css';
+import Head from 'next/head';
 import { Inter } from 'next/font/google';
 import NavBar from '../components/global/NavBar';
 import Footer from '../components/global/Footer';
@@ -22,13 +23,21 @@ export default function App({ Component, ...rest }) {
   return (
     <Provider store={store}>
       <AuthContextProvider>
-        <main className={inter.className}>
-          <NavBar router={router} />
-          <div className='spacer-top' />
-          <Component {...props.pageProps} router={router} />
-          <div className='spacer-bottom' />
-          <Footer router={router} />
-        </main>
+        <div>
+          <Head>
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            <link rel="manifest" href="/site.webmanifest" />
+          </Head>
+          <main className={inter.className}>
+            <NavBar router={router} />
+            <div className='spacer-top' />
+            <Component {...props.pageProps} router={router} />
+            <div className='spacer-bottom' />
+            <Footer router={router} />
+          </main>
+        </div>
       </AuthContextProvider>
     </Provider>
   )
